b0ce05c5ce40cb8d878e10c0dfaf155ba3baca66
ramabit <f.e.ramirez94@gmail.com>
2014-09-14 19:07:07 -0300

    empezando a modelar msp

---
+++ MSP/src/pagina.h
@@ -0,0 +1,23 @@
+/*
+ * pagina.h
+ *
+ *  Created on: 14/09/2014
+ *      Author: utnso
+ */
+
+#ifndef PAGINA_H_
+#define PAGINA_H_
+
+#include <commons/collections/list.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <direccion.h>
+
+typedef struct pagina {
+	uint16_t id;
+	bool tiene_marco;
+	bool en_disco;
+	uint32_t marco;
+} pagina_t;
+
+#endif /* PAGINA_H_ */
