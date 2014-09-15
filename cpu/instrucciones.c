89fbd602e99c26467e45b1a14a8a9917df5d8841
santiago <saantii.28@hotmail.com>
2014-09-15 18:48:15 -0300

    Agrego instruccion SUBR

--- cpu/instrucciones.c
+++ cpu/instrucciones.c
@@ -91,6 +91,11 @@ int addr(tcb_t* tcb) {
 }
 
 int subr(tcb_t* tcb) {
+	char* registro1 = obtener_registro(tcb);
+	char* registro2 = obtener_registro(tcb);
+	int valor1 = obtener_valor_de_registro(tcb, registro1);
+	int valor2 = obtener_valor_de_registro(tcb, registro2);
+	tcb->a = valor1 - valor2;
 	return 0;
 }
 
