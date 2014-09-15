28a93396c78e8fd006bbb739eab782602b840c7d
Martin Montenegro <martin.92m@gmail.com>
2014-09-15 18:51:40 -0300

    Agregados comentarios. Borrado MSP vacio

--- kernel/src/serviciosacpu.c
+++ kernel/src/serviciosacpu.c
@@ -12,10 +12,12 @@
 
 int crear_hilo(tcb_t* tcb, uint32_t tid)
 {
+	// Creamos TCB
 	tcb_t* tcb_hilo = crear_tcb(tcb->pid, tid);
 
+	// Le asignamos la misma base_codigo y un nuevo base_stack
 	if(cargar_tcb_sin_codigo(tcb, tcb_hilo) != -1)
-	{
+	{// Hay memoria, agregamos a RDY
 		agregar_a_ready(tcb_hilo);
 		return 0;
 	}
