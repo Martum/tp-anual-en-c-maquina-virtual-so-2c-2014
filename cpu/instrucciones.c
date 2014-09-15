9726863e1884a674ad56a27019c89172c7600b9a
santiago <saantii.28@hotmail.com>
2014-09-15 15:35:59 -0300

    Agrego intruccion load

---
+++ cpu/instrucciones.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int load(tcb_t tcb) {
+	int res;
+
+	// Pido a memoria 1 byte del registro a partir de tcb->pc y lo guardo en registro
+	char* registro = malloc(sizeof(char));
+	registro = leer_memoria(tcb->pc, sizeof(char));
+	tcb.pc = tcb.pc + 1;
+
+	// Pido a memoria 4 byte del numero a partir de tcb->pc y lo guardo en numero
+	int32_t numero = leer_memoria(tcb->pc, sizeof(int32_t));
+	tcb.pc = tcb.pc + 4;
+
+	switch (registro) {
+	case 'a':
+		tcb.a = numero;
+		break;
+	case 'b':
+		tcb.b = numero;
+		break;
+	case 'c':
+		tcb.c = numero;
+		break;
+	case 'd':
+		tcb.d = numero;
+		break;
+	case 'e':
+		tcb.e = numero;
+		break;
+	default:
+		return -1;
+	}
+	return 0;
+}
+
---
+++ cpu/instrucciones.h
@@ -0,0 +1,13 @@
+/*
+ * instrucciones.h
+ *
+ *  Created on: 15/09/2014
+ *      Author: utnso
+ */
+
+#ifndef INSTRUCCIONES_H_
+#define INSTRUCCIONES_H_
+
+int load(tcb_t tcb);
+
+#endif /* INSTRUCCIONES_H_ */
