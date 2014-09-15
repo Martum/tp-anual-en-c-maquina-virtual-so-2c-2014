1edc20d6821362ac3f10a9a94165bee2c1b4e905
Martin Montenegro <martin.92m@gmail.com>
2014-09-15 16:28:57 -0300

    Agregado manejo de Queues para el Planificador

---
+++ kernel/src/lstestados.c
@@ -0,0 +1,45 @@
+/*
+ * lstestados.c
+ *
+ *  Created on: 15/9/2014
+ *      Author: martin
+ */
+
+#include "lstestados.h"
+#include <stdbool.h>
+#include <commons/collections/queue.h>
+
+t_queue* ready[2];
+t_queue* exec;
+t_queue* block;
+t_queue* exit;
+
+void inicializar_listas()
+{
+	exec = queue_create();
+	block = queue_create();
+	exit = queue_create();
+
+	ready[0] = queue_create();
+	ready[1] = queue_create();
+}
+
+void agregar_a_ready(tcb_t* tcb)
+{
+	queue_push(ready[!tcb->km], tcb);
+}
+
+void agregar_a_block(tcb_t* tcb)
+{
+	queue_push(block, tcb);
+}
+
+void agregar_a_exec(tcb_t* tcb)
+{
+	queue_push(exec, tcb);
+}
+
+void agregar_a_exit(tcb_t* tcb)
+{
+	queue_push(exit, tcb);
+}
