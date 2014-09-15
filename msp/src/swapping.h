02142beb5e4f39f662b81e4ed4ae1ac3523dbc52
utnso <ink_94@hotmail.com>
2014-09-15 18:40:42 -0300

    Empezado swap. No compila, tiene errores. A modificar

---
+++ msp/src/swapping.h
@@ -0,0 +1,20 @@
+/*
+ * swapping.h
+ *
+ *  Created on: 15/09/2014
+ *      Author: utnso
+ */
+
+
+#include "marco.h"
+#include "pagina.h"
+
+#ifndef SWAPPING_H_
+#define SWAPPING_H_
+
+
+void swap(pagina_t*);
+
+marco_t* liberar_un_marco();
+
+#endif /* SWAPPING_H_ */
