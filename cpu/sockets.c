833fb70660bc33a0bb75faed9574403cc1ab2331
santiago <saantii.28@hotmail.com>
2014-09-15 16:18:09 -0300

    Agrego funcion leer_memoria a sockets

--- cpu/sockets.c
+++ cpu/sockets.c
@@ -5,6 +5,7 @@
  *      Author: utnso
  */
 #include <stdio.h>
+#include "estructuras.h"
 
 int conectar_con_memoria() {
 	printf("Me conecto con la memoria MSP. Si falla la conexion devuelvo -1\n");
@@ -16,7 +17,12 @@ int conectar_con_kernel() {
 	return 0;
 }
 
-int pido_tcb(tcb_t tcb, int* quantum) {
+int pedir_tcb(tcb_t tcb, int* quantum) {
 	printf("Mientras no me de un tcb devuelvo -1");
+	return 0;
+}
+
+int leer_de_memoria(direccion pc, int bytes, char* buffer) {
+	// tendria que devolver la cantidad de byes que leyo o algo asi
 	return 0;
 }
--- cpu/sockets.h
+++ cpu/sockets.h
@@ -5,11 +5,14 @@
  *      Author: utnso
  */
 
+#include "estructuras.h"
+
 #ifndef MEMORIA_H_
 #define MEMORIA_H_
 
 int conectar_con_memoria();
 int conectar_con_kernel();
-int pido_tcb(tcb_t tcb, int* quantum);
+int pedir_tcb(tcb_t tcb, int* quantum);
+int leer_de_memoria(direccion tcb, int bytes, char* buffer);
 
 #endif /* MEMORIA_H_ */
