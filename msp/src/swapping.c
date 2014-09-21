fa01c1efa7b9123b50c1e598416a0438d7ad6fc2
Narcos2W <ink_94@hotmail.com>
2014-09-21 11:01:20 -0300

    Desarrollada parcialmente la funcion mover a disco

--- msp/src/swapping.c
+++ msp/src/swapping.c
@@ -17,7 +17,7 @@ uint16_t id_segmento;
 
 uint16_t id_pagina;
 
-/*
+
 void swap_in(pagina_t* pagina)
 {
 	marco_t* marco_libre = liberar_un_marco();
@@ -32,7 +32,7 @@ void swap_in(pagina_t* pagina)
 
  marco_t* liberar_un_marco()
 {
-	marco_t* marco_a_liberar= realizar_algoritmo_swapping(pid, id_segmento, id_pagina);
+	marco_t* marco_a_liberar= realizar_algoritmo_swapping();
 
 	mover_a_disco(marco_a_liberar);
 
@@ -45,13 +45,21 @@ marco_t* realizar_algoritmo_swapping()
 
 
 }
-*/
+
 
 
 void mover_a_disco(marco_t* marco)
 {
 	//ACA FALTA DECIRLE A LA PAGINA QUE ACTUALMENTE OCUPA EL MARCO QUE VA A ESTAR EN DISCO
 	//PAGINA.EN_DISCO=TRUE
+
+	//Como mierda obtengo una pagina?
+
+	//crea un archivo y lo guarda en una carpeta interna.
+	//el nombre se compone de pid, idsegmento y id pagina
+	FILE* arch = txt_open_for_append("en_disco/bla.txt");
+
+	//txt_write_in_file(arch, "Estoy escribiendo!");
 
 
 
