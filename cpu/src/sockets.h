69fd1efb7030bdbe7d059c59d9c6c92c98859e91
santiago <saantii.28@hotmail.com>
2014-09-22 13:51:42 -0300

    Agrego descripcion a funciones de sockets.h

--- cpu/src/sockets.h
+++ cpu/src/sockets.h
@@ -20,8 +20,23 @@ int32_t destruir_segmento(tcb_t* tcb, direccion direccion);
 int32_t leer_de_memoria(direccion dir, int32_t bytes, void* buffer);
 int32_t escribir_en_memoria(direccion dir, int32_t byes, void* buffer);
 int32_t informar_a_kernel_de_finalizacion(tcb_t* tcb, resultado_t res);
+
+/*
+ * @DESC: Lee de memoria los primeros 4 bytes apuntados por pc y actualiza instruccion.
+ * @RETURNS:
+ */
 void obtener_instruccion(tcb_t* tcb, instruccion_t instruccion);
+
+/*
+ * @DESC: Lee de memoria un byte apuntado por pc.
+ * @RETURNS: el char leido
+ */
 char obtener_registro(tcb_t* tcb);
+
+/*
+ * @DESC: Lee de memoria los primeros 4 bytes apuntados por pc.
+ * @RETURNS: el numero leido
+ */
 int32_t obtener_numero(tcb_t* tcb);
 
 #endif /* MEMORIA_H_ */
