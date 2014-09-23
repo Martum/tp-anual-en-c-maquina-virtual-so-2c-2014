834035d0f7aa3742529d22b0c1ff89167b5330fe
santiago <saantii.28@hotmail.com>
2014-09-23 16:51:35 -0300

    Agrego archivo sockets-estructuras.h

---
+++ cpu/src/sockets-estructuras.h
@@ -0,0 +1,15 @@
+/*
+ * sockets-estructuras.h
+ *
+ *  Created on: 23/09/2014
+ *      Author: utnso
+ */
+
+#ifndef SOCKETS_ESTRUCTURAS_H_
+#define SOCKETS_ESTRUCTURAS_H_
+
+typedef struct mensaje {
+	uint32_t flag;
+} mensaje_t;
+
+#endif /* SOCKETS_ESTRUCTURAS_H_ */
