1e51bff468c39d6cc815a4dedae3714903d53841
santiago <saantii.28@hotmail.com>
2014-09-24 18:29:36 -0300

    Agrego todas las estructuras de mensajes que faltaban

--- libs/hu4sockets/mensajes.h
+++ libs/hu4sockets/mensajes.h
@@ -13,8 +13,7 @@
 
 typedef uint32_t direccion;
 
-typedef struct tcb
-{
+typedef struct tcb {
 	uint32_t pid;
 	uint32_t tid;
 	bool km;
@@ -23,19 +22,77 @@ typedef struct tcb
 	direccion pc;
 	direccion base_stack;
 	direccion cursor_stack;
-	int32_t a,b,c,d,e,f;
+	int32_t a, b, c, d, e, f;
 } tcb_t;
 
-typedef enum {MANDA_TCB, TOMA_TCB} flag_t;
+typedef enum {
+	EXITO,
+	MANDA_TCB,
+	TOMA_TCB,
+	CREAME_UN_SEGMENTO,
+	TOMA_SEGMENTO,
+	ERROR_MEMORIA_LLENA,
+	DESTRUI_SEGMENTO,
+	LEE_DE_MEMORIA,
+	ERROR_VIOLACION_DE_SEGMENTO,
+	ESCRIBI_EN_MEMORIA,
+	TOMA_RESULTADO,
+} flag_t;
 
 typedef struct mensaje_request {
 	flag_t flag;
 } mensaje_request_t;
+
+typedef struct mensaje_response {
+	flag_t flag;
+} mensaje_response_t;
+
+typedef struct mensaje_request_resultado {
+	flag_t flag;
+	uint32_t resultado;
+} mensaje_request_resultado_t;
+
+typedef struct mensaje_request_crear_segmento {
+	flag_t flag;
+	uint32_t pid;
+	uint32_t tamano;
+} mensaje_request_crear_segmento_t;
+
+typedef struct mensaje_request_destruir_segmento {
+	flag_t flag;
+	uint32_t pid;
+	direccion direccion_virtual;
+} mensaje_request_destruir_segmento_t;
+
+typedef struct mensaje_request_lee_de_memoria {
+	flag_t flag;
+	uint32_t pid;
+	direccion direccion_virtual;
+	uint32_t tamano;
+} mensaje_request_lee_de_memoria_t;
+
+typedef struct mensaje_request_escribi_en_memoria {
+	flag_t flag;
+	uint32_t pid;
+	direccion direccion_virtual;
+	char* bytes_a_escribir;
+	uint32_t tamano;
+} mensaje_request_escribi_en_memoria_t;
 
 typedef struct mensaje_response_tcb {
 	flag_t flag;
 	tcb_t tcb;
 	uint32_t quantum;
 } mensaje_response_tcb_t;
+
+typedef struct mensaje_response_crear_segmento {
+	flag_t flag;
+	direccion direccion_virtual;
+} mensaje_response_crear_segmento_t;
+
+typedef struct mensaje_response_lee_de_memoria {
+	flag_t flag;
+	char* bytes_leido;
+} mensaje_response_lee_de_memoria_t;
 
 #endif /* MENSAJES_H_ */
