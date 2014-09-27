1469674f8147294aa78e8d528d5995924e0189f6
santiago <saantii.28@hotmail.com>
2014-09-27 17:47:31 -0300

    Creo un archivo tcb para manejar los tcb en las libs

---
+++ libs/hu4sockets/tcb.c
@@ -0,0 +1,80 @@
+/*
+ * tcb.c
+ *
+ *  Created on: 27/09/2014
+ *      Author: utnso
+ */
+#include "tcb.h"
+#include <stdlib.h>
+
+tcb_t* crear_tcb() {
+	return (tcb_t*) malloc(sizeof(tcb_t));
+}
+
+void liberar_tcb(tcb_t* tcb) {
+	free(tcb);
+}
+
+resultado_t actualizar_valor_en_registro(tcb_t* tcb, char registro, int32_t numero) {
+	if ('a' == registro) {
+		tcb->a = numero;
+		return OK;
+	}
+	if ('b' == registro) {
+		tcb->a = numero;
+		return OK;
+	}
+	if ('c' == registro) {
+		tcb->a = numero;
+		return OK;
+	}
+	if ('d' == registro) {
+		tcb->a = numero;
+		return OK;
+	}
+	if ('e' == registro) {
+		tcb->a = numero;
+		return OK;
+	}
+	return NO_ENCONTRO_EL_REGISTRO;
+}
+
+resultado_t obtener_valor_de_registro(tcb_t* tcb, char registro,
+		int32_t* numero) {
+	if ('a' == registro) {
+		*numero = tcb->a;
+		return OK;
+	}
+	if ('b' == registro) {
+		*numero = tcb->b;
+		return OK;
+	}
+	if ('c' == registro) {
+		*numero = tcb->c;
+		return OK;
+	}
+	if ('d' == registro) {
+		*numero = tcb->d;
+		return OK;
+	}
+	if ('e' == registro) {
+		*numero = tcb->e;
+		return OK;
+	}
+	return NO_ENCONTRO_EL_REGISTRO;
+}
+
+int32_t actualizar_pc(tcb_t* tcb, int32_t valor) {
+	tcb->pc = valor;
+	return OK;
+}
+
+int32_t obtener_pc(tcb_t* tcb) {
+	return tcb->pc;
+}
+
+direccion obtener_base_de_codigo(tcb_t* tcb) {
+	return tcb->base_codigo;
+}
+
+
