cbf9102bfb84608eebffbffd132050bac8b0c789
santiago <saantii.28@hotmail.com>
2014-09-27 17:47:01 -0300

    Agrego un header resultados con una enum de resultados posibles

---
+++ libs/hu4sockets/resultados.h
@@ -0,0 +1,13 @@
+/*
+ * resultados.h
+ *
+ *  Created on: 27/09/2014
+ *      Author: utnso
+ */
+
+#ifndef RESULTADOS_H_
+#define RESULTADOS_H_
+
+typedef enum {OK, FALLO, NO_ENCONTRO_EL_REGISTRO, TERMINO, FALLO_CONEXION} resultado_t;
+
+#endif /* RESULTADOS_H_ */
