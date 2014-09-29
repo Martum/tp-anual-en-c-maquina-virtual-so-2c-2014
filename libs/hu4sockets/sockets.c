3883db55c4ed4676aa29e8ddd9bbebcb576bb52f
Martin Montenegro <martin.92m@gmail.com>
2014-09-29 15:47:42 -0300

    Formateando el codigo

--- libs/hu4sockets/sockets.c
+++ libs/hu4sockets/sockets.c
@@ -1,9 +1,11 @@
 /*
  * sockets.c
  *
- * Created on: 16/9/2014
- * Author: martin
+ *  Created on: 16/9/2014
+ *      Author: martin
  */
+
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -11,43 +13,45 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+
 #include "sockets.h"
 
 
 sock_t* _crear_socket()
 {
 	sock_t* sock = (sock_t*) malloc(sizeof(sock_t));
-	sock->datos_conexion = (struct sockaddr_in*) malloc(
-			sizeof(struct sockaddr));
+	sock->datos_conexion = (struct sockaddr_in*) malloc(sizeof(struct sockaddr));
+
 	sock->fd = socket(AF_INET, SOCK_STREAM, 0);
+
 	return sock;
 }
 
-
 void _preparar_conexion(sock_t* socket, char* ip, uint32_t puerto)
 {
-// Seteamos la IP, si es NULL es localhost
-	if (ip == NULL)
+	// Seteamos la IP, si es NULL es localhost
+	if(ip == NULL)
 		socket->datos_conexion->sin_addr.s_addr = htonl(INADDR_ANY); // Local
 	else
 		socket->datos_conexion->sin_addr.s_addr = inet_addr(ip);	// Remota
+
 	socket->datos_conexion->sin_family = AF_INET;
 	socket->datos_conexion->sin_port = htons(puerto);
 	memset(&(socket->datos_conexion->sin_zero), '\0', 8);
 }
 
-
 sock_t* _crear_y_preparar(char* ip, uint32_t puerto)
 {
 	sock_t* socket = _crear_socket();
 	_preparar_conexion(socket, ip, puerto);
+
 	return socket;
 }
 
 
 int32_t _limpiar_puerto(sock_t* socket)
 {
-	int yes = 1;
+	int yes=1;
 	return setsockopt(socket->fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 }
 
@@ -55,8 +59,7 @@ int32_t _limpiar_puerto(sock_t* socket)
 int32_t _bind_puerto(sock_t* socket)
 {
 	_limpiar_puerto(socket);
-	return bind(socket->fd, (struct sockaddr *) socket->datos_conexion,
-			sizeof(struct sockaddr));
+	return bind(socket->fd, (struct sockaddr *)socket->datos_conexion, sizeof(struct sockaddr));
 }
 
 
@@ -65,23 +68,24 @@ int32_t _enviar(sock_t* socket, char* msg, uint32_t len)
 	return send(socket->fd, msg, len, 0);
 }
 
-
 int32_t _enviar_todo(sock_t* socket, char* msg, uint32_t* len)
 {
-	int32_t total = 0; // cuantos bytes hemos enviado
+	int32_t total = 0;        // cuantos bytes hemos enviado
 	int32_t bytesleft = *len; // cuantos se han quedado pendientes
 	int32_t n;
-	while (total < *len)
-	{
-		n = _enviar(socket, msg + total, bytesleft);
-// Si falla el envio cortamos el loop
+	while(total < *len) {
+		n = _enviar(socket, msg+total, bytesleft);
+
+		// Si falla el envio cortamos el loop
 		if (n == -1)
 			break;
+
 		total += n;
 		bytesleft -= n;
 	}
+
 	*len = total; // devuelve aqui la cantidad enviada en realidad
-	return n == -1 ? -1 : 0; // devuelve -1 si hay fallo, 0 en otro caso
+	return n==-1?-1:0; // devuelve -1 si hay fallo, 0 en otro caso
 }
 
 
@@ -96,18 +100,23 @@ int32_t _recibir_todo(sock_t* socket, char* buff, uint32_t* len)
 	int32_t recibidos = 0;
 	int32_t n = 0;
 	int32_t bytesleft = *len;
-	while (recibidos < *len)
+
+	while(recibidos < *len)
 	{
 		n = _recibir(socket, buff, *len);
-// Si falla la recepcion cortamos el loop
+
+		// Si falla la recepcion cortamos el loop
 		if (n == -1)
 			break;
+
 		recibidos += n;
 		bytesleft -= n;
 	}
+
 	*len = recibidos;
-	return n == -1 ? -1 : 0;
+	return n == -1?-1:0;
 }
+
 
 
 void _cerrame_esto_nestor(sock_t* socket)
@@ -140,6 +149,7 @@ cabecera_t* _crear_cabecera(uint32_t len)
 	cabecera_t* cabecera = _crear_cabecera_vacia();
 	cabecera->longitud_mensaje = len;
 	cabecera->valido = CABECERA_VALIDA;	// Seteamos variable para ver si los datos llegan bien
+
 	return cabecera;
 }
 
@@ -148,6 +158,7 @@ char* _serializar_cabecera(cabecera_t* cabecera)
 {
 	char* msg = malloc(_tamanio_cabecera());
 	memcpy(msg, cabecera, _tamanio_cabecera());
+
 	return msg;
 }
 
@@ -161,8 +172,9 @@ char* _crear_cabecera_serializada(uint32_t len)
 int32_t _deserealizar_cabecera(cabecera_t* cabecera, char* bytes)
 {
 	memcpy(cabecera, bytes, _tamanio_cabecera());
-// Si la cabecera verifica la validacion, se recibio bien
-	return (cabecera->valido == CABECERA_VALIDA) ? 0 : -1;
+
+	// Si la cabecera verifica la validacion, se recibio bien
+	return (cabecera->valido == CABECERA_VALIDA)?0:-1;
 }
 
 
@@ -170,23 +182,30 @@ int32_t _recibir_cabecera(sock_t* socket, cabecera_t* cabecera)
 {
 	char* bytes_cabecera = malloc(_tamanio_cabecera());
 	uint32_t i = _tamanio_cabecera();
-// Si no se recibe la cabecera se tira error
-	if (_recibir_todo(socket, bytes_cabecera, &i) == -1)
+
+	// Si no se recibe la cabecera se tira error
+	if(_recibir_todo(socket, bytes_cabecera, &i) == -1)
 		return -1;
-// Si no se puede deserealizar la cabecera o esta es invalida
-	if (_deserealizar_cabecera(cabecera, bytes_cabecera) == -1)
+
+	// Si no se puede deserealizar la cabecera o esta es invalida
+	if(_deserealizar_cabecera(cabecera, bytes_cabecera) == -1)
 		return -1;
+
 	return 0;
 }
 
 
 /**** FUNCIONES PUBLICAS ****/
+
+
 sock_t* crear_socket_escuchador(uint32_t puerto)
 {
 	sock_t* socket = _crear_y_preparar(NULL, puerto);
-// Si el puerto esta ocupado, devolvemos NULL
-	if (_bind_puerto(socket) == -1)
+
+	// Si el puerto esta ocupado, devolvemos NULL
+	if(_bind_puerto(socket) == -1)
 		return NULL;
+
 	return socket;
 }
 
@@ -199,8 +218,7 @@ sock_t* crear_socket_hablador(char* ip, uint32_t puerto)
 
 int32_t conectar(sock_t* socket)
 {
-	return connect(socket->fd, (struct sockaddr *) socket->datos_conexion,
-			sizeof(struct sockaddr));
+	return connect(socket->fd, (struct sockaddr *)socket->datos_conexion, sizeof(struct sockaddr));
 }
 
 
@@ -213,26 +231,31 @@ int32_t escuchar(sock_t* socket)
 sock_t* aceptar_conexion(sock_t* socket)
 {
 	sock_t* sock_nuevo = _crear_socket();
-	uint32_t i = sizeof(struct sockaddr_in);// TODO: Ver que hacer con este INT. Sirve devolverlo dentro del struct?
-	sock_nuevo->fd = accept(socket->fd,
-			(struct sockaddr *) sock_nuevo->datos_conexion, &i);
-	return sock_nuevo->fd == -1 ? NULL : sock_nuevo;
+	uint32_t i = sizeof(struct sockaddr_in);	// TODO: Ver que hacer con este INT. Sirve devolverlo dentro del struct?
+
+	sock_nuevo->fd = accept(socket->fd, (struct sockaddr *)sock_nuevo->datos_conexion, &i);
+
+	return sock_nuevo->fd == -1?NULL:sock_nuevo;
 }
 
 
 int32_t enviar(sock_t* socket, char* msg, uint32_t* len)
 {
-// Seteamos el Header
+	// Seteamos el Header
 	uint32_t len_cabecera = _tamanio_cabecera();
 	char* cabecera_serializada = _crear_cabecera_serializada(*len);
-// Enviamos el Header, si falla devolvemos -1
-	if (_enviar_todo(socket, cabecera_serializada, &len_cabecera) == -1)
+
+	// Enviamos el Header, si falla devolvemos -1
+	if(_enviar_todo(socket, cabecera_serializada, &len_cabecera) == -1)
 		return -1;
-// Liberamos cabecera
+
+	// Liberamos cabecera
 	free(cabecera_serializada);
-// Enviamos el Mensaje, si falla devolvemos -1
-	if (_enviar_todo(socket, msg, len) == -1)
+
+	// Enviamos el Mensaje, si falla devolvemos -1
+	if(_enviar_todo(socket, msg, len) == -1)
 		return -1;
+
 	return 0;
 }
 
@@ -240,17 +263,22 @@ int32_t enviar(sock_t* socket, char* msg, uint32_t* len)
 int32_t recibir(sock_t* socket, char** msg, uint32_t* len)
 {
 	cabecera_t* cabecera = _crear_cabecera_vacia();
-// Si no se recibe la cabecera se tira error
-	if (_recibir_cabecera(socket, cabecera) == -1)
+
+	// Si no se recibe la cabecera se tira error
+	if(_recibir_cabecera(socket, cabecera) == -1)
 		return -1;
-// Creamos un buffer del tamaño necesario
+
+	// Creamos un buffer del tamaño necesario
 	*msg = malloc(cabecera->longitud_mensaje);
 	*len = cabecera->longitud_mensaje;
-// Liberamos cabecera, ya no la necesitamos
+
+	// Liberamos cabecera, ya no la necesitamos
 	free(cabecera);
-// Si no se recibe el mensaje completamente fallamos, pero dejamos lo leido en el buffer
-	if (_recibir_todo(socket, *msg, len) == -1)
+
+	// Si no se recibe el mensaje completamente fallamos, pero dejamos lo leido en el buffer
+	if(_recibir_todo(socket, *msg, len) == -1)
 		return -1;
+
 	return 0;
 }
 
@@ -260,7 +288,6 @@ void cerrar_liberar(sock_t* socket)
 	_cerrame_esto_nestor(socket);
 	_liberar_memoria(socket);
 }
-
 
 void cerrar_liberar_generico(void* elemento)
 {
