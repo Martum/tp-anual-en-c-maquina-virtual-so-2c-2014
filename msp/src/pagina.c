f34859bfc95c52a5eb342bd76632b8a9ab2760a2
ramabit <f.e.ramirez94@gmail.com>
2014-09-29 16:20:08 -0300

    agrego campo ultima_vez_usada en pagina

--- msp/src/pagina.c
+++ msp/src/pagina.c
@@ -11,22 +11,15 @@
 #include <commons/collections/list.h>
 #include "segmento.h"
 
-
-//CAMBIAR LO QUE DEVUELVE
-void crear_pagina(segmento_t *segmento)
-{
+void crear_pagina(segmento_t *segmento){
 	pagina_t *pagina = malloc(sizeof(pagina_t));
 	pagina->tiene_marco = false;
 	pagina->en_disco = false;
 
 	agregar_pagina_a_segmento(segmento,pagina);
 	pagina->id = list_size(segmento->paginas)-1;
-
 }
 
-//CAMBIAR LO QUE DEVUELVE
-void agregar_pagina_a_segmento(segmento_t *segmento, pagina_t *pagina)
-{
+void agregar_pagina_a_segmento(segmento_t *segmento, pagina_t *pagina){
 	list_add(segmento->paginas, pagina);
-
 }
