4e4d3435e3774b2aff0871df7df27333a7fad67a
MatiasSas94 <sas.matiasn@gmail.com>
2014-09-29 16:35:54 -0300

    modificaciones en mensajes y planificador.

--- kernel/src/planificador.c
+++ kernel/src/planificador.c
@@ -5,6 +5,19 @@
  *      Author: utnso
  */
 
+#include "planificador.h"
+#include <hu4sockets/mensajes.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <commons/collections/list.h>
+#include <hu4sockets/sockets.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include "configuraciones.h"
+#include "conexiones.h"
+#include <hu4sockets/mensajes.h>
 
 tcb_t* _planificar(){
 
@@ -23,12 +36,16 @@ tcb_t* _planificar(){
 respuesta_de_nuevo_tcb_t  devolver_tcb(){
 	respuesta_de_nuevo_tcb_t rta;
 
+	flag_t flag = RESPONDO_TCB;
+
 	rta.tcb = _planificar();
 	rta.quantum = quantum();
-	rta.flag = 251;
+	rta.flag = RESPONDO_TCB;
 
 	return rta;
 }
 
-void recibir_tcb(pedido_con_resultado_t);
+void recibir_tcb(pedido_con_resultado_t resultado){
+	if (resultado.flag == TOMA_RESULTADO)
+}
 
