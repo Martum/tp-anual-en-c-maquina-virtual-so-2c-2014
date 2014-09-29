6d547fa61ce253be0fe849eddf535bb8d63d9d76
santiago <saantii.28@hotmail.com>
2014-09-29 19:09:47 -0300

    En la CPU anda todo

--- cpu/src/sockets.c
+++ cpu/src/sockets.c
@@ -47,10 +47,10 @@ resultado_t pedir_tcb(sock_t** kernel, tcb_t* tcb, int32_t* quantum) {
 
 resultado_t pedir_tcb_compartido(sock_t** kernel, tcb_t* tcb, int32_t* quantum) {
 	printf("Me preparo para mandar\n");
-	mensaje_request_t cuerpo_del_mensaje;
+	pedido_t cuerpo_del_mensaje;
 	cuerpo_del_mensaje.flag = MANDA_TCB;
-	char* mensaje_a_enviar = malloc(sizeof(mensaje_request_t));
-	uint32_t len_a_enviar = sizeof(mensaje_request_t);
+	char* mensaje_a_enviar = malloc(sizeof(pedido_t));
+	uint32_t len_a_enviar = sizeof(pedido_t);
 	memcpy(mensaje_a_enviar, &cuerpo_del_mensaje, len_a_enviar);
 	printf("Envio\n");
 	enviar(*kernel, mensaje_a_enviar, &len_a_enviar);
