dc6d893dc82df8989d2fbf5addde73ee7843f785
Martin Montenegro <martin.92m@gmail.com>
2014-10-12 19:35:28 -0300

    Agregadas funciones para puerto e ip de kernel

--- consola/src/configuraciones.c
+++ consola/src/configuraciones.c
@@ -14,3 +14,13 @@ void cargar_configuraciones(void)
 {
 	configs = config_create("config_consola.txt");
 }
+
+char* ip_kernel()
+{
+	return config_get_string_value(configs, "IP_KERNEL");
+}
+
+uint32_t puerto_kernel()
+{
+	return config_get_int_value(configs, "PUERTO_KERNEL");
+}
