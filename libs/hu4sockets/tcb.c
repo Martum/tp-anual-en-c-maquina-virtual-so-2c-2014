cab92ef9ee8633dd130fc2d2309c20dd45e9a504
santiago <saantii.28@hotmail.com>
2014-10-13 15:07:57 -0300

    Correccion de errores de tcb.c en libs

--- libs/hu4sockets/tcb.c
+++ libs/hu4sockets/tcb.c
@@ -7,18 +7,8 @@
 
 
 #include "tcb.h"
-
-typedef struct tcb {
-	uint32_t pid;
-	uint32_t tid;
-	bool km;
-	direccion base_codigo;
-	direccion tamanio_codigo;
-	direccion pc;
-	direccion base_stack;
-	direccion cursor_stack;
-	int32_t a, b, c, d, e, f;
-} tcb_t;
+#include <stdlib.h>
+#include <string.h>
 
 tcb_t* deserializar_tcb(char* chorro)
 {
