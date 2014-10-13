936a86cb4eefd6af3a917a6668c4b8adbc9734f9
Martin Montenegro <martin.92m@gmail.com>
2014-10-13 17:34:45 -0300

    Cambios para que compile kernel

--- kernel/src/lstestados.c
+++ kernel/src/lstestados.c
@@ -16,16 +16,16 @@
 t_queue* block;*/
 
 t_queue* ready[2];
-t_queue* exit;
+t_queue* exit_cola;
 t_list* exec;
 t_list* block;
 t_dictionary* dic_colas_espera_recursos;
-t_queue* syscalls;
+t_queue* syscalls_cola;
 
 void inicializar_listas_estados_tcb() {
 
-	exit = queue_create();
-	syscalls = queue_create();
+	exit_cola = queue_create();
+	syscalls_cola = queue_create();
 
 	ready[0] = queue_create();
 	ready[1] = queue_create();
@@ -52,16 +52,16 @@ void agregar_a_block(tcb_t* tcb) {
 	list_add(block, tcb);
 }
 
-void agregar_a_syscalls(tcb_t* tcb) {
-	queue_push(syscalls, tcb);
+void agregar_a_syscalls_cola(tcb_t* tcb) {
+	queue_push(syscalls_cola, tcb);
 }
 
 void agregar_a_exec(tcb_t* tcb) {
 	list_add(exec, tcb);
 }
 
-void agregar_a_exit(tcb_t* tcb) {
-	queue_push(exit, tcb);
+void agregar_a_exit_cola(tcb_t* tcb) {
+	queue_push(exit_cola, tcb);
 }
 
 void quitar_de_exec(tcb_t* tcb) {
@@ -82,16 +82,16 @@ void quitar_de_block(tcb_t* tcb) {
 	list_remove_by_condition(block, _igual_tid);
 }
 
-void agregar_a_cola_recurso(uint32_t recurso, tcb_t* tcb) {
-	queue_push(dictionary_get(dic_colas_espera_recursos, recurso), tcb);
+void agregar_a_cola_recurso(char* recurso, tcb_t* tcb) {
+	queue_push((t_queue*)dictionary_get(dic_colas_espera_recursos, recurso), tcb);
 }
 
-void quitar_de_syscalls(tcb_t* tcb) {
-	queue_pop(syscalls);
+void quitar_de_syscalls_cola(tcb_t* tcb) {
+	queue_pop(syscalls_cola);
 }
 
-tcb_t* quitar_primero_de_cola_recurso(uint32_t recurso){
-	return queue_pop(dictionary_get(dic_colas_espera_recursos, recurso));
+tcb_t* quitar_primero_de_cola_recurso(char* recurso){
+	return queue_pop((t_queue*)dictionary_get(dic_colas_espera_recursos, recurso));
 }
 
 tcb_t* quitar_de_ready_km(){
