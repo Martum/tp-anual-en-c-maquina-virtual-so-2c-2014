a3878f99a8937bec18bcde945ee9dc861d60183e
santiago <saantii.28@hotmail.com>
2014-10-15 21:02:51 -0300

    Arreglos en main de cpu Ya no pregunta por quantum -1 para ver si el de kernel si no que utiliza el campo km del tcb Elimino algunos comentarios innecesarios

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -6,17 +6,17 @@ int32_t main(int32_t argc, char** argv)
 	setvbuf(stdout, NULL, _IONBF, 0); // funcion necesiaria para imprimir en pantalla en eclipse
 
 	if (conectar_con_memoria() == FALLO_CONEXION
-		|| conectar_con_kernel() == FALLO_CONEXION) { // todo informar por pantalla y log
+		|| conectar_con_kernel() == FALLO_CONEXION) { // TODO informar por pantalla y log
 		printf("ERROR FALTAL: Fallo la conexion\n");
 		return 0;
 	}
 
-	printf("Se pudo conectar a memoria y kernel\n"); // todo convertir a log
+	printf("Se pudo conectar a memoria y kernel\n"); // TODO convertir a log
 
 	tcb_t tcb;
 	t_dictionary* dic_instrucciones = dictionary_create();
 	resultado_t (*funcion)(tcb_t*);
-	resultado_t res = OK;
+	resultado_t resultado = OK;
 	int32_t quantum;
 	instruccion_t instruccion;
 
@@ -24,41 +24,41 @@ int32_t main(int32_t argc, char** argv)
 
 	while (1) {
 		if (pedir_tcb(&tcb, &quantum) == FALLO_PEDIDO_DE_TCB)
-			res = ERROR_EN_EJECUCION;
+			resultado = ERROR_EN_EJECUCION;
 
 		/*
 		 dictionary_destroy(dic_instrucciones);
 		 cerrar_puertos();
-		 printf("ERROR FALTAL: Fallo pedido de tcb\n"); // todo no deberia romper sino avisar a kernel
+		 printf("ERROR FALTAL: Fallo pedido de tcb\n");
 		 return 0;
 		 */
 
 		printf("%d\n", quantum); // TODO eliminar (solamente para pruebas)
 		printf("%d\n", tcb.a); // TODO eliminar (solamente para pruebas)
 
-		break;
+		break; // TODO eliminar (solamente para pruebas)
 
-		if (quantum < -1 || quantum == 0)
-			res = ERROR_EN_EJECUCION;
+		if ((quantum <= 0) && !tcb.km)
+			resultado = ERROR_EN_EJECUCION;
 
 		/*
 		 dictionary_destroy(dic_instrucciones);
 		 cerrar_puertos();
 		 printf("ERROR FALTAL: Quantum invalido. Aborto\n");
-		 break; // todo no deberia romper si no avisar a kernel
+		 break;
 		 */
 
-		while ((quantum > 0 || quantum == -1) && res == OK) { // Quantum -1 significa que es el kernel
+		while ((quantum > 0 || tcb.km) && resultado == OK) {
 			obtener_instruccion(&tcb, &instruccion);
 			funcion = dictionary_get(dic_instrucciones, instruccion);
-			res = funcion(&tcb);
+			resultado = funcion(&tcb);
 			quantum--;
 		}
 
-		if (res == OK)
-			res = FIN_QUANTUM;
+		if (resultado == OK)
+			resultado = FIN_QUANTUM;
 
-		informar_a_kernel_de_finalizacion(tcb, res);
+		informar_a_kernel_de_finalizacion(tcb, resultado);
 	}
 
 	dictionary_destroy(dic_instrucciones);
