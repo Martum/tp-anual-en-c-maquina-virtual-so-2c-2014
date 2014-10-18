b9db70e7fe3aecbf266d89882adc0115b73055b0
ramabit <f.e.ramirez94@gmail.com>
2014-10-17 23:23:58 -0300

    agrego struct de conexiones de cpus

--- msp/src/conexiones_cpu.h
+++ msp/src/conexiones_cpu.h
@@ -8,6 +8,11 @@
 #ifndef CONEXIONES_CPU_H_
 #define CONEXIONES_CPU_H_
 
+typedef struct conexion_cpu {
+	sock_t* socket;
+	uint32_t id;
+} conexion_cpu_t;
+
 void inicializar_lista_conexiones_cpu();
 void* escuchar_cpus(void*);
 
