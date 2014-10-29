e67552449af81bd8eb800ca2e4b02f10f402d31a
ramabit <f.e.ramirez94@gmail.com>
2014-10-29 15:20:13 -0300

    corrijo bug al crear segmento

--- msp/src/proceso_msp.h
+++ msp/src/proceso_msp.h
@@ -11,7 +11,7 @@
 #include "estructuras.h"
 
 void listar_segmentos_de_un_proceso(proceso_msp_t*);
-proceso_msp_t* crear_proceso_msp();
+proceso_msp_t* crear_proceso_msp(uint32_t);
 proceso_msp_t* buscar_proceso_segun_pid(uint32_t);
 bool quitar_segmento(proceso_msp_t*, direccion);
 void _destruye_segmento(segmento_t*);
