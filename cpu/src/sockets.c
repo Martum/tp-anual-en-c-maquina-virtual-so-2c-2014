e48e64f4f3436ab408016a85a81ea77338751c8a
santiago <saantii.28@hotmail.com>
2014-11-03 19:31:07 -0300

    Agrego mas loggeos a sockets cpu

--- cpu/src/sockets.c
+++ cpu/src/sockets.c
@@ -188,7 +188,7 @@ resultado_t crear_segmento(direccion pid, uint32_t bytes, direccion* direccion)
 	char* chorro_de_respuesta = malloc(
 		tamanio_respuesta_de_crear_segmento_t_serializado());
 
-	loggear_trace("Me preparo para enviar pedido de creacion de segmento");
+	loggear_trace("Me preparo crear segmento");
 
 	loggear_debug("PID %d -- Tamanio del segmento %d", pid, bytes);
 
@@ -201,7 +201,7 @@ resultado_t crear_segmento(direccion pid, uint32_t bytes, direccion* direccion)
 		return FALLO_CREACION_DE_SEGMENTO;
 	}
 
-	loggear_trace("Recibi una respuesta por creacion de segmento");
+	loggear_trace("Recibi respuesta por creacion de segmento");
 
 	respuesta_de_crear_segmento_t* respuesta =
 		deserializar_respuesta_de_crear_segmento_t(chorro_de_respuesta);
@@ -212,18 +212,18 @@ resultado_t crear_segmento(direccion pid, uint32_t bytes, direccion* direccion)
 	if (respuesta->resultado == ERROR_DE_MEMORIA_LLENA)
 	{
 		loggear_warning(
-			"La creacion no se pudo realizar porque la memoria estaba llena");
+			"La creacion no pudo realizarse porque la memoria estaba llena");
 		free(respuesta);
 		return FALLO_CREACION_DE_SEGMENTO;
 	}
 
-	loggear_info("Creacion de segmento satisfactoria");
-
 	*direccion = respuesta->direccion_virtual;
 
-	loggear_debug("Direccion del segmento %d", *direccion);
-
 	free(respuesta);
+
+	loggear_info("Creacion de segmento satisfactoria");
+
+	loggear_debug("Direccion del segmento %d", *direccion);
 
 	return OK;
 }
@@ -239,7 +239,7 @@ resultado_t destruir_segmento(direccion pid, direccion direccion)
 		&cuerpo_del_mensaje);
 	char* chorro_de_respuesta = malloc(tamanio_respuesta_t_serializado());
 
-	loggear_trace("Me preparo para enviar pedido de destruccion de segmento");
+	loggear_trace("Me preparo destruir segmento");
 
 	loggear_debug("PID: %d -- Direccion %d", pid, direccion);
 
@@ -252,7 +252,7 @@ resultado_t destruir_segmento(direccion pid, direccion direccion)
 		return FALLO_DESTRUCCION_DE_SEGMENTO;
 	}
 
-	loggear_trace("Recibi una respuesta por destruccion de segmento");
+	loggear_trace("Recibi respuesta por destruccion de segmento");
 
 	respuesta_t* respuesta = deserializar_respuesta_t(chorro_de_respuesta);
 
@@ -261,14 +261,15 @@ resultado_t destruir_segmento(direccion pid, direccion direccion)
 
 	if (respuesta->resultado == ERROR_NO_ENCUENTRO_SEGMENTO)
 	{
-		loggear_warning("La destruccion no se pudo realizar porque no encontro el segmetno");
+		loggear_warning(
+			"La destruccion no pudo realizarse por no encontrar el segmetno");
 		free(respuesta);
 		return FALLO_DESTRUCCION_DE_SEGMENTO;
 	}
 
-	loggear_info("Destruccion de segmento satisfactoria");
-
 	free(respuesta);
+
+	loggear_info("Destruccion de segmento satisfactoria");
 
 	return OK;
 }
@@ -287,6 +288,10 @@ resultado_t leer_de_memoria(direccion pid, direccion direccion,
 	char* chorro_de_respuesta = malloc(
 		tamanio_respuesta_de_leer_de_memoria_t_serializado(cantidad_de_bytes));
 
+	loggear_trace("Me preparo leer de memoria");
+
+	loggear_debug("Cantidad de bytes a leer %d", cantidad_de_bytes);
+
 	if (_enviar_y_recibir(memoria, chorro_de_envio,
 		tamanio_pedido_de_leer_de_memoria_t_serializado(), chorro_de_respuesta)
 		== FALLO_COMUNICACION)
@@ -296,6 +301,8 @@ resultado_t leer_de_memoria(direccion pid, direccion direccion,
 		return FALLO_LECTURA_DE_MEMORIA;
 	}
 
+	loggear_trace("Recibi respuesta por lectura de memoria");
+
 	respuesta_de_leer_de_memoria_t* respuesta =
 		deserializar_respuesta_de_leer_de_memoria_t(chorro_de_respuesta);
 
@@ -304,15 +311,21 @@ resultado_t leer_de_memoria(direccion pid, direccion direccion,
 
 	if (respuesta->resultado == SEGMENTATION_FAULT)
 	{
+		loggear_warning(
+			"La lectura no pudo realizarse por violacion de segmento");
 		free(respuesta->bytes_leido);
 		free(respuesta);
 		return FALLO_LECTURA_DE_MEMORIA;
 	}
 
-	memcpy(buffer, respuesta->bytes_leido, cantidad_de_bytes);
+	memcpy(buffer, respuesta->bytes_leido, respuesta->tamano);
 
 	free(respuesta->bytes_leido);
 	free(respuesta);
+
+	loggear_info("Lectura de memorira satisfactoria");
+
+	loggear_debug("Cantidad de bytes leidos %d", respuesta->tamano);
 
 	return OK;
 }
@@ -331,6 +344,11 @@ resultado_t escribir_en_memoria(direccion pid, direccion direccion,
 		&cuerpo_del_mensaje);
 	char* chorro_de_respuesta = malloc(tamanio_respuesta_t_serializado());
 
+	loggear_trace("Me preparo escribir en memoria");
+
+	loggear_debug("Cantidad de bytes a escribir %d -- Bytes %s",
+		cantidad_de_bytes, bytes_a_escribir);
+
 	if (_enviar_y_recibir(memoria, chorro_de_envio,
 		tamanio_pedido_de_escribir_en_memoria_t_serializado(cantidad_de_bytes),
 		chorro_de_respuesta) == FALLO_COMUNICACION)
@@ -340,6 +358,8 @@ resultado_t escribir_en_memoria(direccion pid, direccion direccion,
 		return FALLO_ESCRITURA_EN_MEMORIA;
 	}
 
+	loggear_trace("Recibi respuesta por escritura en memoria");
+
 	respuesta_t* respuesta = deserializar_respuesta_t(chorro_de_respuesta);
 
 	free(chorro_de_envio);
@@ -347,11 +367,15 @@ resultado_t escribir_en_memoria(direccion pid, direccion direccion,
 
 	if (respuesta->resultado == SEGMENTATION_FAULT)
 	{
+		loggear_warning(
+			"La escritura no pudo realizarse por violacion de segmento");
 		free(respuesta);
 		return FALLO_ESCRITURA_EN_MEMORIA;
 	}
 
 	free(respuesta);
+
+	loggear_info("Escritura en memoria satisfactoria");
 
 	return OK;
 }
