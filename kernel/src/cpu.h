b5b3dcf93a4d929579b4a449637f65f574018097
MatiasSas94 <sas.matiasn@gmail.com>
2014-11-10 16:38:22 -0300

    Bugfixes en de/serializar varios

--- kernel/src/cpu.h
+++ kernel/src/cpu.h
@@ -118,6 +118,11 @@ typedef struct segmentos_por_hilo {
  */
 void agregar_segmentos_por_hilo(segmentos_por_hilo_t* segmentos);
 
+/*
+ * Quita el segmento que se corresponde con la direccion virtual recibida,
+ * del hilo correspondiente.
+ */
+void quitar_segmento_de_hilo(uint32_t pid, uint32_t tid, direccion dir_virtual);
 
 
 #endif /* SERVICIOSACPU_H_ */
