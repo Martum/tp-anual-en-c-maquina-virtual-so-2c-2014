8e63a9544e9915f828df060a98802e5620c920cd
MatiasSas94 <sas.matiasn@gmail.com>
2014-11-24 19:05:48 -0300

    Modificaciones en cpu, nuevo destruir memoria en la msp con distinto valor de retorno, para su uso en la cpu.

--- kernel/src/cpu.c
+++ kernel/src/cpu.c
@@ -48,12 +48,27 @@ segmentos_por_hilo_t* find_segmento_de_hilo(uint32_t pid, uint32_t tid) {
 	return list_find(segmentos_por_hilo,_elementoConListaDeSegmentos);
 }
 
+void _enviar_rta_crear_segmento_a_cpu(char* rta_serializada, uint32_t tamanio, uint32_t* cpu_id) {
+
+	sock_t* socket = buscar_conexion_cpu_por_id(*cpu_id);
+	enviar(socket, rta_serializada, &tamanio);
+
+	free(rta_serializada);
+}
+
 /* TODO: Agregar a conexiones.c */
 void crear_segmento_cpu(uint32_t pid, uint32_t tamanio, uint32_t* cpu_id) {
 	direccion* dir = malloc(sizeof(direccion));
 	uint32_t tid = get_tcb_km()->tid;
 
-	if (crear_segmento(pid, tamanio, dir) == 0) {
+	uint32_t tamanio_rta = tamanio_respuesta_de_crear_segmento_t_serializado();
+	char * rta_serializada = malloc(tamanio_rta);
+	rta_serializada = crear_segmento_retornando_rta_serializada(pid, tamanio, dir);
+
+	respuesta_de_crear_segmento_t rta_deserializada = deserializar_respuesta_de_crear_segmento_t(rta_serializada);
+
+	// TODO: REVISAR QUE LA REPSUESTA CORRECTA SEA RESULTADO_OK
+	if (rta_deserializada.resultado == RESULTADO_OK) {
 		segmentos_por_hilo_t* segmentos = find_segmento_de_hilo(pid,tid);
 		if (segmentos == NULL){
 			segmentos_por_hilo_t* segmentos = malloc(sizeof(segmentos_por_hilo_t));
@@ -62,11 +77,17 @@ void crear_segmento_cpu(uint32_t pid, uint32_t tamanio, uint32_t* cpu_id) {
 			segmentos->segmentos = list_create();
 		}
 		list_add(segmentos->segmentos, dir);
-		/* TODO: NOTIFICAR A SANTI QUE SE PUDO CREAR EL SEGMENTO */
+
+		/* TODO: NOTIFICAR A SANTI QUE SE PUDO CREAR EL SEGMENTO. CREO QUE LA RESPUESTA ES IGUAL
+		 * YA SEA TOD BIEN O TOD MAL, Y ES LA QUE YA NOS MANDA LA MSP CUANDO LE MANDAMOS
+		 * CREAR SEGMENTO. */
 	} else {
-		/*TODO: NOTIFIACAR A SANTI QUE NO SE PUDO CREAR EL SEGMENTO */
+		/*TODO: NOTIFIACAR A SANTI QUE NO SE PUDO CREAR EL SEGMENTO. CREO QUE LA RESPUESTA ES IGUAL
+		 * YA SEA TOD BIEN O TOD MAL, Y ES LA QUE YA NOS MANDA LA MSP CUANDO LE MANDAMOS
+		 * CREAR SEGMENTO. */
 	}
 
+	_enviar_rta_crear_segmento_a_cpu(rta_serializada, tamanio_rta, cpu_id);
 }
 
 void _enviar_rta_destruir_segmento_a_cpu(char* rta_serializada, uint32_t tamanio, uint32_t* cpu_id) {
